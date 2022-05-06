0216473a96bd616d00d8c6fe257d919b044db6fa
Erik <d21erisu@student.his.se>
2022-05-06 10:00:00 +0200

    added button that opens a second activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences prefRef;
     private SharedPreferences.Editor prefRefEditor;
     private TextView showText;
+    private Button openButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +22,25 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        //  startActivity(intent);
+
         prefRef = getPreferences(MODE_PRIVATE);
         prefRefEditor = prefRef.edit();
 
-
         showText= findViewById(R.id.mainTextView);
         showText.setText(prefRef.getString("MyAppPreferenceString", "No preference found."));
+
+        openButton=findViewById(R.id.mainButton);
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 }
