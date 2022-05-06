aca4f65c6f5155cbc2139b4ddade0f14022debe7
Erik <d21erisu@student.his.se>
2022-05-06 09:37:48 +0200

    added textview and read data from shared pref

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences prefRef;
+    private SharedPreferences.Editor prefRefEditor;
+    private TextView showText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        prefRef = getPreferences(MODE_PRIVATE);
+        prefRefEditor = prefRef.edit();
+
+
+        showText= findViewById(R.id.mainTextView);
+        showText.setText(prefRef.getString("MyAppPreferenceString", "No preference found."));
     }
 
 }
